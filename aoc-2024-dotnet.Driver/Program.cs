2d4d785a092615b7c5b8fa3d8c5a29f478bbe6af
josh <joshwhtny@yahoo.com>
2024-12-09 14:06:40 -0800

    re-organize project to fix tests

--- aoc-2024-dotnet.Driver/Program.cs
+++ aoc-2024-dotnet.Driver/Program.cs
@@ -1,13 +1,13 @@
-﻿using aoc_2024_dotnet.Day1;
+﻿using aoc_2024_dotnet.Library.Day1;
 
-namespace aoc_2024_dotnet;
+namespace aoc_2024_dotnet.Driver;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-        Solution s = new Solution("./src/Day1/input.txt");
+        Solution s = new Solution("./Assets/Day1.txt");
         Console.WriteLine($"Solution 1 Total is : {s.Solve()}");
         Console.WriteLine($"Solution 2 Total is : {s.Solve2()}");
     }
