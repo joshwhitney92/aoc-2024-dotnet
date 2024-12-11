280a3ba2a849c48f2f52fc8e6b91f05cffb11261
josh <joshwhtny@yahoo.com>
2024-12-11 11:57:41 -0800

    Add helper methods

--- aoc-2024-dotnet.Library/Solutions/Day3/Helper.cs
+++ aoc-2024-dotnet.Library/Solutions/Day3/Helper.cs
@@ -3,6 +3,15 @@ namespace aoc_2024_dotnet.Library.Solutions.Day3;
 public static class Helper
 {
 
+
+    public static void WriteToFile(IEnumerable<string> list) {
+        StreamWriter writer = new StreamWriter("output_disabled.txt");
+        foreach(string line in list) {
+            writer.Write(line);
+        }
+
+        writer.Flush();
+    }
     public static IEnumerable<string> ReadToList(string filepath)
     {
         StreamReader reader = new StreamReader(filepath);
